ae3b1c41f83cfdbcf287fcd0bde2937ceb0dc7dc
rehorn <sysucgh@163.com>
2012-05-02 08:23:25 +0800

    demo init

---
+++ demo/common/js/main.js
@@ -0,0 +1,25 @@
+
+
+;(function(){
+    var navTree = [
+        {
+            id: 'Core',
+            name: 'Core',
+            url: '',
+            children: [
+                {
+                    id: 'Loader'
+                }
+            ]
+
+        },
+    ];
+
+    var jxDemo = {
+        init: function(){
+
+        }
+    };
+
+    jxDemo.init();
+});
